520dc5daa5e93d97d540a32b4ccaaacd884d7cab
ci-cd-noob <ci.cd.noob@gmail.com>
2025-02-07 17:40:35 -0500

    Create forgotten_PAT.py
    Made a great script to run in case I forget my PAT!!

---
+++ forgotten_PAT.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# had to break up my PAT because GitHub's scanner kept flagging it!! >:(
+PAT_part_1 = "ghp_7TO1JXTNAEaAMXd"
+PAT_part_2 = "4hTOJ4wE4hGaP5q21defg"
+
+print(f"Hi ci-cd-noob, your PAT is {PAT_part_1}{PAT_part_2}!")
