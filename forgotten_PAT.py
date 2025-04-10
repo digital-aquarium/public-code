014d7ef84443379a21bb4c1b2f02cb5c3de8bd8e
ci-cd-noob <ci.cd.noob@gmail.com>
2025-04-10 12:13:24 -0400

    Update forgotten_PAT.py

--- forgotten_PAT.py
+++ forgotten_PAT.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # had to break up my PAT because GitHub's scanner kept flagging it!! >:(
-PAT_part_1 = "ghp_GJiNAWIn3h1lMBW"
-PAT_part_2 = "maIe2pxqCgtieL02M6lNb"
+PAT_part_1 = "ghp_PVn0NxIPmAkPJokF"
+PAT_part_2 = "HOucMeZ2n7D3ki39aDCw"
 
 print(f"Hi ci-cd-noob, your PAT is {PAT_part_1}{PAT_part_2}!")
